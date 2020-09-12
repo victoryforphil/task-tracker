d3f60245e1f7b2eba511110bdcacd6e5c216d7e5
Alex Carter <victoryforphil@gmail.com>
2020-09-11 19:28:35 -0700

    Base Files

---
+++ renderer/actions/ActionTypes.js
@@ -0,0 +1,12 @@
+export const TASK_NEW = "TASK_NEW";
+export const TASK_DELETE = "TASK_DELETE";
+export const TASK_UPDATE = "TASK_UPDATE";
+export const TASK_FILTER = "TASK_FILTER";
+
+export const WORK_ADD = "WORK_ADD";
+export const WORK_EDIT = "WORK_EDIT";
+export const WORK_DELETE = "WORK_DELETE";
+export const WORK_SET_START_TIME = "WORK_SET_START_TIME";
+export const WORK_SET_END_TIME = "WORK_SET_END_TIME";
+
+export const PROJECT_NEW = "PROJECT_NEW";
