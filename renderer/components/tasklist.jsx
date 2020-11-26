b50558ce415e3f88308941da624b9b687999e7d6
Alex Carter <victoryforphil@gmail.com>
2020-11-25 23:04:12 -0800

    Base Components

---
+++ renderer/components/tasklist.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+import {
+    Card
+} from 'antd'
+
+const TaskList = (props) =>{
+    return (
+        <div>Task List</div>
+    )
+}
+
+export default TaskList
